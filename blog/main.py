2e50c742125c97fd8a5cbfdd2a6cc5ad9ecd615d
Yash Tank <62990171+yashtank09@users.noreply.github.com>
2022-04-24 15:35:43 +0530

    Used Password Hasing and created another pydentic model

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Depends, Response, status
+from typing import List
+from fastapi import FastAPI, Depends, Response, status, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from .hashing import Hash
 
 app = FastAPI()
 
@@ -25,17 +27,63 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def getallblog(db: Session = Depends(get_db)):
     all_blogs = db.query(models.Blog).all()
     return all_blogs
 
 
-@app.get('/blog{id}', status_code=200)
+@app.delete('/blog/{id}')
+def deleteBlog(id, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not deleteBlog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail={'data': f"Blog with id {id} is not available"})
+
+    deleteBlog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
+@app.put('/bog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, req: schemas.Blog, db: Session = Depends(get_db)):
+    updateBlog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not updateBlog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail={'data': f"Blog with id {id} is not available"})
+
+    updateBlog.update(req.dict())  # Ned convert Blog schema object to dictionary
+    db.commit()
+    return 'Updated'
+
+
+@app.get('/blog{id}', status_code=200, response_model=schemas.ShowBlog)
 def showBlog(id, res: Response, db: Session = Depends(get_db)):
     showBlog = db.query(models.Blog).filter(models.Blog.id == id).first()
 
     if not showBlog:
-        res.status_code = status.HTTP_200_OK
-        return {'data': f"Blog with id {id} is not available"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail={'data': f"Blog with id {id} is not available"})
+        # res.status_code = status.HTTP_200_OK
+        # return {'data': f"Blog with id {id} is not available"}
     return showBlog
+
+
+@app.post('/user')
+def create_users(req: schemas.User, db: Session = Depends(get_db)):
+    newUser = models.User(name=req.name, email=req.email, password=Hash.bcrypt(req.password))
+    db.add(newUser)
+    db.commit()
+    db.refresh(newUser)
+    return newUser
+
+
+@app.get('/user{id}', status_code=200, response_model=schemas.User)
+def showUser(id, res: Response, db: Session = Depends(get_db)):
+    showBlog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
+    if not showBlog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail={'data': f"Blog with id {id} is not available"})
